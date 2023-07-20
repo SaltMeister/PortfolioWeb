32920d8d103ffd62197ccd7fbb0bf5c527302e81
Simon Huang <huisimonliu@gmail.com>
2023-07-19 23:41:48 -0700

    Updated the sidebar animations

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,16 +7,16 @@ import SideBarComponent from './components/SideBarComponent';
 function App() {
   const desc1 = "A simple game of checkers created using react.";
   const desc2 ="An android game where you defend your graphics cards from bitcoin miners.";
-  const desc3 ="test";
+  const desc3 ="A Music player created with C# and the .Net framework";
   const desc4 = "A website created for CitroHacks 2023 that uses the OpenAI api to generate weekly plans for the user. Created using React, Flask, and Tailwind CSS.";
-  const desc5 ="A website that allows users to search and save recipes. Users can sign up and keep their data. Created using React, Flask, Tailwind, and MongoDB."
+  const desc5 ="A website that allows users to search and save recipes. Users can sign up and keep their data. Worked on the user authentication between the client and server/database. Created using React, Flask, Tailwind, and MongoDB."
   return (
     <div className="App">
       <HeaderComponent headerText="Portfolio"/>
       <SideBarComponent arrayOfHeaders={["Home", "Projects", "Resume"]}/>
 
       <DisplayBoxComponent>
-        <DisplayComponent title="Pantry Pal" description={desc5} link=""/>
+        <DisplayComponent title="Pantry Pal" description={desc5} link="https://github.com/Jeffuz/Pantry-Pals"/>
         <DisplayComponent title="FitFlow" description={desc4} link="https://devpost.com/software/fitflow-pacuwy"/>
         <DisplayComponent title="Checkers" description={desc1} link="https://saltmeister.github.io/checkers/" />
         <DisplayComponent title="Bitcoin Defenders" description={desc2} link="https://github.com/SaltMeister/BitcoinDefender"/>
--- portfolio/src/components/SideBarComponent.jsx
+++ portfolio/src/components/SideBarComponent.jsx
@@ -3,14 +3,16 @@ import styles from '../css/SideBar.module.css';
 
 export default function SideBarComponent({arrayOfHeaders}) {
   const [isDisplayText, setIsDisplayText] = useState(false);
+  const [isPopupCollision, setIsPopupCollision] = useState(false);
+
   console.log(isDisplayText);
   return(
-    <div class={styles.sideBarWrapper}>
+    <div class={styles.sideBarWrapper}
+    onMouseEnter={() => setIsDisplayText(true)} 
+    onMouseLeave={() => setIsDisplayText(false)}>
       
       {/* ICONS */}
-      <div class={styles.sideBarContainer} 
-      onMouseEnter={() => setIsDisplayText(true)} 
-      onMouseLeave={() => setIsDisplayText(false)}>
+      <div class={styles.sideBarContainer} >
         
           {arrayOfHeaders.map((index, key) => 
             <div key={key} class={styles.sideBarItem}> 
@@ -20,13 +22,16 @@ export default function SideBarComponent({arrayOfHeaders}) {
       </div> 
       
       {/* Hold TEXT */}
+      {isDisplayText? (
       <div class={`${styles.sideBarContainer} ${styles.sideBarPopupContainer} ${(isDisplayText ? (styles.display) : (styles.hide))}`}>
       {arrayOfHeaders.map((index, key) => 
         <div key={key} class={styles.sideBarItem} > 
             <p>{index} </p> 
         </div>
       )}
-    </div>        
+    </div>   ) : (<div/>)}
+      
+     
 
 
     </div>
