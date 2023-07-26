272ab55f172bec406eb71283498f825ac8ab2561
Simon Huang <huisimonliu@gmail.com>
2023-07-26 13:44:18 -0700

    Animations for projects section

--- portfolio/src/components/DisplayComponent.jsx
+++ portfolio/src/components/DisplayComponent.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "../css/Display.module.css";
 
 export default function DisplayComponent({title, description, link}) {
   
+  const [isHover, setIsHover] = useState(false);
   const goToLink = (e) => {
     window.open(link, "Null");
   }
-
+  const setFalse = () => {
+    setIsHover(false);
+  }
+  const setTrue = () => {
+    setIsHover(true);
+  }
   return(
-    <div class={styles.displayItem} onClick={goToLink}>
+    <div class={styles.displayItem} onClick={goToLink}
+        onMouseEnter={setTrue} onMouseLeave={setFalse}>
+
+
       <h3 class={styles.title}>{title}</h3>
       <p class={styles.text}>{description}</p>
 
-      <svg width="100" height="100">
-        <circle cx="50" cy="50" r="20" stroke="green" stroke-width="2" fill="ivory"/>
-      </svg>
+      <div >
+        {isHover? (        
+        
+          <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class={styles.arrow} viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+          </svg>) : 
+          
+          (<div/>)}
+   
+      </div>
+
     </div>
   )
 
