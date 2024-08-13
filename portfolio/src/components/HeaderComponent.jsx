199147b171a7e9aaf4572d361027c114278ad5e6
Simon Huang <huisimonliu@gmail.com>
2024-08-12 17:07:18 -0700

    Fixed Resume opening

--- portfolio/src/components/HeaderComponent.jsx
+++ portfolio/src/components/HeaderComponent.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import styles from "../css/Header.module.css";
-
+import pdf from "../Simon_Resume.pdf";
 export default function HeaderComponent({arrayOfHeaders}){
+  const handleClick = (value) => {
+    if(value === "Resume") {
+      window.open(pdf, "_blank")
+      return
+    }  
+
+    window.scrollTo({top: document.getElementById(value).offsetTop, behavior: "smooth"})
+  }
+  
   return(
     <div className={styles.name}>
       <div className={styles.rightItems}>
         {arrayOfHeaders.map((value, key) => {
           return(
-            <button className={styles.header} onClick={() => 
-              window.scrollTo({top: document.getElementById(value).offsetTop, behavior: "smooth"})}
-              key={key}>{value}</button>
+            <button className={styles.header} onClick={() => handleClick(value)}
+              key={key}>
+                {value}
+            </button>
           )
         })}        
       </div>
